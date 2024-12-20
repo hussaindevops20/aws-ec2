dd956c40d772923b877860094bf09e1a47c7df98
vt-devops20 <learningdevops20@gmail.com>
2024-12-20 21:02:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     count = 2
-    tags {
+    tags = {
       Name = "ec2 ${count.index}"
     }
 }
