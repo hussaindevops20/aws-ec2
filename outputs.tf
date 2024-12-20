ab8b2f68dc379cc8a977131ab9a827905e37e6e8
vt-devops20 <learningdevops20@gmail.com>
2024-12-20 21:06:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public-ip-address" {
-  value = aws_instance.ec2.public_ip
+  value = aws_instance.ec2[count.index].public-ip
 }
 
 output "instance_id" {
-  value = aws_instance.ec2.id
+  value = aws_instance.ec2[count.index].id
 }
