2029da936b10f31d47761949d52c842d607f63b5
vt-devops20 <learningdevops20@gmail.com>
2024-12-20 20:53:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "ami_value" {
-    description = "ami-06b21ccaeff8cd686"
+    default = "ami-06b21ccaeff8cd686"
 }
 
 variable "instance_type_value" {
-    description = "t2.micro"
+    default = "t2.micro"
 }
 
 variable "region_value" {
-    description ="us-east-1"
+    default ="us-east-1"
 }
 
